95c173e11b66a5cf843e17ea72f66188d6e26bae
Abhijeet mane <abhi.mane07012000@gmail.com>
2022-12-28 11:28:39 +0530

    Review Section Done.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Heading from '../../UI/Heading/Heading'
 import Coursel from '../../UI/Coursel/Coursel'
+import HomeCard from '../../UI/HomeCard/HomeCard'
 
 const Home = () => {
   return (
     <>
     <Coursel />
     <Heading  heading="💖Let Goooo for Shopping🛒"/>
+    <HomeCard headings={['MAYA','EMMA','GRACE']}/>
     </>
   )
 }
---
+++ src/UI/HomeCard/HomeCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./HomeCard.css";
+import CardImage from "../../Assets/1.png";
+
+const HomeCard = (props) => {
+  return (
+    <>
+      <section className="review">
+        <div className="row">
+          {props.headings.map((heading) => {
+            return (
+              <div className="col-lg-4">
+                <div className="card h-100">
+                  <div className="card-header">
+                    <img className="w-75" src={CardImage} alt="" />
+                  </div>
+                  <div className="card-body">
+                    <h4>{heading}</h4>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Praesent egestas arcu sed risus efficitur sollicitudin in
+                      quis elit.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeCard;
